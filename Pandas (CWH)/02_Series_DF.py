0bead448695f7928613a01b2ea63c1ef335748af
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-11-16 09:07:44 +0600

    Series

--- Pandas (CWH)/02_Series_DF.py
+++ Pandas (CWH)/02_Series_DF.py
@@ -25,3 +25,26 @@ print(data_frame.sort_index(axis=1, ascending=False))
 
 # Dataframe is created using combination of Series
 print(type(data_frame[0]))
+
+
+
+
+# VIEW: View is a shallow copy of the dataframe,
+# any changes made in the view will be reflected in the original dataframe
+# In memory location, both the dataframes will be directed to the same location
+# new_df = data_frame.copy()
+# new_df[0][0] = 0.5
+
+
+#  .loc and .iloc
+print(data_frame.iloc[0, 0])
+# This will return the value at 0th row and 0th column
+# whatever is the index, it will be considered as the row and column number
+
+print(data_frame.loc[0, 0])
+# This will return the value at 0th row and 0th column
+# Actual names should be used as row and column names
+
+# Query
+print(data_frame.loc[(data_frame[0] > 0.5) & (data_frame[3] > 0.5)])
+# This checks conditions on column 0 and 3 and returns the rows which satisfy the condition
