ab5c4034cea9b0f9d6d859be311e8e25d1e53fd3
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-11-16 07:09:53 +0600

    LAB01

--- LAB_01 (Python Basics)/basics.py
+++ LAB_01 (Python Basics)/basics.py
@@ -46,4 +46,41 @@ student_dict = {
 for key,value in student_dict.items():
   print(key, value)
   
-  
+# Creating a Student Data Set dictionary  
+student_data_set = {
+    "name": ["student1", "student2", "student3"],
+    "id" : ["2022-1", "2022-2", "2022-3"],
+    "CGPA" : [3.55, 4.0, 3.9]
+}
+
+print(student_data_set["name"])
+
+for key,value in student_data_set.items():
+  print(key, ":")
+  for val in value:
+    print(val)
+
+# Creating a Employee Data Set dictionary & performing operations
+employee_data_set = {
+    "Name" : ["Emp1", "Emp2", "Emp3"],
+    "Salary": [20000, 40000, 60000],
+    "Dept": ["HR", "IT", "HR"]
+}
+print(employee_data_set)
+# add a new column named City
+employee_data_set["City"] = ["Dhaka", "Khulna", "Barishal"]
+
+print(employee_data_set)
+
+# Delete "IT" from Dept
+for i in range(0, 2, 1):
+  if employee_data_set["Dept"][i] == "IT":
+     del employee_data_set["Dept"][i]
+
+print(employee_data_set)
+# Update Emp1 Salary to 100000
+for i in range(0, len(employee_data_set["Name"]), 1):
+  if employee_data_set["Name"][i] == "Emp1":
+    employee_data_set["Salary"][i] = 100000
+
+print(employee_data_set)    
