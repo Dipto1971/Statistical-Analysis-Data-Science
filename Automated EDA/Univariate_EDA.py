f2e97700ce5c23223f6b90cb396a5cee2a9f04c7
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-12-26 16:30:19 +0600

    Fix variable naming for quantiles in univariate EDA function

--- Automated EDA/Univariate_EDA.py
+++ Automated EDA/Univariate_EDA.py
@@ -18,17 +18,17 @@ def univariate(df):
     if pd.api.types.is_numeric_dtype(df[col]):
       # Calculate metrics that apply only to numeric features
       min = df[col].min()
-      Q1 = df[col].quantile(0.25)
+      q1 = df[col].quantile(0.25)
       median = df[col].median()
-      Q3 = df[col].quantile(0.75)
+      q3 = df[col].quantile(0.75)
       max = df[col].max()
       mean = df[col].mean()
       std = df[col].std()
       skewness = df[col].skew()
       kurtosis = df[col].kurtosis()
 
-      output_df.loc[col] = [dtype, count, missing, unique, mode, min, Q1,
-                            median, Q3, max, mean, std, skewness, kurtosis ]
+      output_df.loc[col] = [dtype, count, missing, unique, mode, min, q1,
+                            median, q3, max, mean, std, skewness, kurtosis ]
 
       sns.histplot(data=df, x=col)     
       plt.show()              
